43c4a8c4e4e797138c5c75620cc99c0347ca219c
Nguyendiu <nguyendiupro15@gmail.com>
2020-11-11 13:25:52 +0700

    save ti lam tiep

---
+++ src/Component/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../Asset/Header.css"
+import '../emojiList.json'
+import '../Asset/Header.css'
+export function Header(props) {
+        return(
+            <div>
+            <h1 className = "title">
+            😀 Search for your favorite emoji 😀
+            </h1>
+            </div>
+        )
+}
